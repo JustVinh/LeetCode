eabe330046ca889a083a4c371233256bb4b24938
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-02 20:07:00 +0700

    Time: 20 ms (57.28%), Space: 13.2 MB (5.36%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-        for(int i =0; i< nums.size() -1; i++){
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
-            }
+        set<int> s(nums.begin(), nums.end());
+        int no_num = nums.size();
+        unordered_map<int, int> m;
+
+      for(int i = 0; i < nums.size(); i++) {
+        // Check if we have seen our buddy
+        int buddy = target - nums[i];
+        auto buddyIt = m.find(buddy);
+
+        if(buddyIt != m.end()) {
+          // Buddy found, return answer
+          return vector<int>{ buddyIt->second, i };
         }
+
+        // Buddy not found, insert current num into buddy map
+        m[nums[i]] = i;
+      }
         return res;
     }
 };
