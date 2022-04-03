3065550cd0ca0b5b55e6f844e99682091fc95560
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-03 20:17:28 +0700

    Time: 4 ms (96.10%), Space: 7.8 MB (80.05%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //mark[i] the first appear position of character i 
-        int mark[130];
-        memset(mark, -1, sizeof(mark));
-        if(s == "") return 0;
-        int res = 0;
-        int longest  =1;
+        vector<int> mark(130, -1);
         
+        int res = 0;
+        int start = -1;
         for(int i=0; i< s.length(); i++){
-            //if not duplicated
-            if(mark[s[i]] == -1){
-                res++;
-                //note the first appear position
-                mark[s[i]] = i;
+            //count from start to the duplicated position
+            if(mark[s[i]] > start ){
+                start = mark[s[i]];
             }
-            //if found duplicated
-            else{
-                //update the longest
-                longest = max(longest, res);
-                
-                //find the length of reuseable substring ex. "abcvnbx" then we can use "cvnb"                 when b is duplicated
-                res = i - mark[s[i]];
-                //reset mark of character that not has been used yet in new substring
-                for(int j =0; j <= mark[s[i]]; j++){
-                	if(mark[s[j]] < mark[s[i]]){ // remove this line = wrong
-                		mark[s[j]] = -1;
-                	}
-                }
+                res = max(res, i - start);
                 mark[s[i]] = i;
-            }
+            
         }
         
-        return max(longest, res);
+        return res;
     }
 };
