8cf3633e066a098973251a1f5d00027825ec2c02
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-05 10:50:20 +0700

    Time: 295 ms (38.28%), Space: 8.1 MB (69.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,55 +1,35 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        // true if from i to j is palindromic 
+       int n= s.length();
         bool table[n+1][n+1];
-        //Never forget to memset
         memset(table, 0, sizeof(table));
+        int start =0;
         int longest = 1;
-        //track the start postion of longest subs string
-        int start = 0;
         
-        //length = 1
-        for(int i =0; i< n; i++){
+        for(int i = 0; i <n-1; i++){
             table[i][i] = true;
-        }
-        
-        //length = 2
-        for(int i =0; i <n -1; i++){
-            if(s[i] == s[i+1]) {
+            if(s[i] == s[i+1]){
                 table[i][i+1] = true;
-                longest = 2;
-                //each time longest get updated, we update tracking start position too
                 start = i;
+                longest = 2;
             }
         }
-    
+        table[n-1][n-1] = true;
         
-        //length >= 3
-        for(int k =3; k <= n; k++){
-            // i is start index of substring have k length
-            for(int i =0; i <= n-k; i++){
-                //j is end index of substring have k length
-                int j = i + k -1;
-                
+        for(int k=3; k<=n; k++){
+            for(int i=0; i<= n-k; i++){
+                int j = i+k -1;
                 if(table[i+1][j-1] && s[i] == s[j]){
                     table[i][j] = true;
                     if(k > longest){
-                        longest = k;
                         start = i;
+                        longest = k;
                     }
                 }
             }
         }
         
-        // printf("Start: %d, Length: %d\n", start, longest);
-        
-        string res = "";
-        for(int i =start; i< start+longest; i++){
-            res += s[i];
-        }
-        
-        return res;
+        return s.substr(start, longest);
     }
 };
