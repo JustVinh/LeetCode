c0c46da81ae351ca550a28937e664df9691f1725
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-17 15:08:01 +0700

    Time: 3 ms (98.25%), Space: 6.8 MB (99.31%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> mark(130, -1);
+        int mark[130];
+        memset(mark, -1, sizeof(mark));
         
-        int res = 0;
+        int n = s.length();
         int start = -1;
-        for(int i=0; i< s.length(); i++){
-            //count from start to the first appear position of the duplicated char
-            //only change start if the marked element is after the start position - meaning only change start position if in tracking list have duplicated
-            if(mark[s[i]] > start ){
+        int longest = 0;
+        
+        for(int i=0; i< n; i++){
+            if(mark[s[i]] > start){
                 start = mark[s[i]];
             }
-                res = max(res, i - start);
-                mark[s[i]] = i;
+            longest = max(longest, i - start);
+            mark[s[i]] = i;
+            
             
         }
         
-        return res;
+        return longest;
     }
 };
