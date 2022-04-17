b19072fcc1e05fa20c03c299dcb9848f10b0b65f
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-17 15:21:23 +0700

    Time: 267 ms (40.67%), Space: 11.6 MB (52.31%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-       int n= s.length();
-        bool table[n+1][n+1];
+        int n = s.length();
+        int table[n+1][n+1];
         memset(table, 0, sizeof(table));
-        int start =0;
-        int longest = 1;
         
-        for(int i = 0; i <n-1; i++){
-            table[i][i] = true;
-            if(s[i] == s[i+1]){
-                table[i][i+1] = true;
-                start = i;
+        int start =0;
+        int longest =1;
+        
+        for(int i=0; i<n; i++){
+            table[i][i] = 1;
+        }
+        
+        for(int i=0; i<n-1;i++){
+            if(s[i] == s[i+1]) 
+            {
+                table[i][i+1] = 1;
+                start =i;
                 longest = 2;
             }
         }
-        table[n-1][n-1] = true;
         
-        for(int k=3; k<=n; k++){
-            for(int i=0; i<= n-k; i++){
-                int j = i+k -1;
-                if(table[i+1][j-1] && s[i] == s[j]){
-                    table[i][j] = true;
-                    if(k > longest){
-                        start = i;
+        for(int k =3; k<=n; k++){
+            for(int i = 0; i<= n-k; i++){
+                int j = i + k -1;
+                if(s[i] == s[j] && table[i+1][j-1] == 1){
+                    table[i][j] = 1;
+                    
+                    if( k > longest){
                         longest = k;
+                        start = i;
                     }
                 }
             }
