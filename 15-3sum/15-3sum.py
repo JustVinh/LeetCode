4621d159fb479108d2ac2bc2d265b5087de94742
Thanh Vinh Nguyen <63626726+JustVinh@users.noreply.github.com>
2022-04-17 20:21:54 +0700

    Time: 793 ms (85.24%), Space: 18.1 MB (74.16%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,24 +1,32 @@
-class Solution(object):
-    def threeSum(self, nums):
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        result = []
-        for left in range(len(nums) - 2): # renamed this to left because this will always be the leftmost pointer in the triplet
-            if left > 0 and nums[left] == nums[left - 1]: # this step makes sure that we do not have any duplicates in our result output
-                continue 
-            mid = left + 1 # renamed this to mid because this is the pointer that is between the left and right pointers
-            right = len(nums) - 1
+        
+        n = len(nums)
+        
+        for leftest in range(n - 2):
+            if leftest > 0 and nums[leftest-1]==nums[leftest]:
+                continue
+            mid = leftest+1
+            right = n-1
+            
             while mid < right:
-                curr_sum = nums[left] + nums[mid] + nums[right]
+                curr_sum = nums[leftest]+nums[mid]+nums[right]
+                
                 if curr_sum < 0:
-                    mid += 1 
+                    mid += 1
                 elif curr_sum > 0:
                     right -= 1
                 else:
-                    result.append([nums[left], nums[mid], nums[right]])
-                    while mid < right and nums[mid] == nums[mid + 1]: # Another conditional for not calculating duplicates
+                    res.append([nums[leftest], nums[mid], nums[right]])
+                    
+                    while mid < right and nums[mid] == nums[mid+1]:
                         mid += 1
-                    while mid < right and nums[right] == nums[right - 1]: # Avoiding duplicates check
+                    while mid < right and nums[right] == nums[right-1]:
                         right -= 1
-                    mid += 1
+                
                     right -= 1
-        return result
+                    mid += 1
+        
+        return res
